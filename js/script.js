43d04a073ae4ab34e21141cbbc7f1f640855139a
mahmud035 <mahamudulhasan4148@gmail.com>
2022-07-09 12:18:56 +0600

    write javascript to remove the class from list-item

--- js/script.js
+++ js/script.js
@@ -2,8 +2,16 @@
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-console.log(toggleButton, navbarLinks);
+const listItems = document.querySelectorAll('.list-item');
+console.log(toggleButton, navbarLinks, listItems);
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
+});
+
+listItems.forEach((item) => {
+  // console.log(item);
+  item.addEventListener('click', function () {
+    navbarLinks.classList.remove('active');
+  });
 });
